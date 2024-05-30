27e2423c89f3d0e4f56a858988a95ce3ae696776
Maguette NDIAYE <maguettendiaye1@esp.sn>
2024-05-30 20:18:55 +0000

    init1

--- src/main/java/dgi/esp/ucad/SbCarApplication.java
+++ src/main/java/dgi/esp/ucad/SbCarApplication.java
@@ -1,5 +1,6 @@
 package dgi.esp.ucad;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
